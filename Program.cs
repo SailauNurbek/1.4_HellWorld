721a854c49a9d86379812ad716d9c1b15ecf4795
Sailau Nurbek <112234136+SailauNurbek@users.noreply.github.com>
2024-06-05 18:39:20 +0500

    1.4_HelloWorld

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+    static void Main(string[] args)                     
+        {
+            //Task 1
+            Console.WriteLine("HelloWorld!!!");
+            Console.ReadKey();
+
+            //Task 2
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!! ");
+            Console.ReadLine();
+        }
+    }
+}
